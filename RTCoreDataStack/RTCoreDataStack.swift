2dd484a9027ee628eaed74e29614a7d280363a09
Aleksandar Vacic <av@radianttap.com>
2016-10-24 18:56:46 +0200

    Documented lots of methods

--- RTCoreDataStack/RTCoreDataStack.swift
+++ RTCoreDataStack/RTCoreDataStack.swift
@@ -14,24 +14,38 @@ import CoreData
 public final class RTCoreDataStack {
 	typealias Callback = () -> Void
 
+	/// Until this is true, data store is not available. Do not attempt to access any of the Core Data objects until isReady=true
 	fileprivate(set) var isReady: Bool = false
 
+	/// Managed Model instance used by the stack
 	fileprivate(set) var dataModel: NSManagedObjectModel!
 
+	/// Full URL to the location of the SQLite file
 	fileprivate(set) var storeURL: URL!
 
+	/// Instantiates the whole stack, giving you full control over what model to use and where the resulting file should be.
+	///
+	/// - parameter dataModelName: String representing the name (without extension) of the model file to use. If not supplied,
+	/// - parameter storeURL: Full URL where to create the .sqlite file. Must include the file at the end as well (can't be just directory). If not supplied, user's Documents directory will be used + alphanumerics from app's name. Possible use: when you want to setup the store file into completely custom location. Like say shared container in App Group.
+	/// - parameter callback: A block to call once setup is completed. RTCoreDataStack.isReady is set to true before callback is executed.
+	///
+	/// - returns: Instance of RTCoreDataStack
 	init(withDataModelNamed dataModel: String? = nil, storeURL: URL? = nil, callback: @escaping Callback = {_ in}) {
 		DispatchQueue.main.async { [unowned self] in
 			self.setup(withDataModelNamed: dataModel, storeURL: storeURL, callback: callback)
 		}
 	}
 
+	/// Instance of PersistentStoreCoordinator intended for main thread's contexts
 	fileprivate(set) var mainCoordinator: NSPersistentStoreCoordinator!
 
+	/// Instance of PersistentStoreCoordinator intended for background thread's importing.
 	fileprivate(set) var writerCoordinator: NSPersistentStoreCoordinator!
 
+	/// Main MOC, connected to mainCoordinator. Use it for all the UI
 	fileprivate(set) var mainContext: NSManagedObjectContext!
 
+	/// Make main MOC read-only and thus prevent
 	var isMainContextReadOnly: Bool = false {
 		didSet {
 			if !isReady { return }
@@ -40,6 +54,7 @@ public final class RTCoreDataStack {
 		}
 	}
 
+	/// Enable or disable automatic merge between importerMOCs and mainMOC
 	var shouldMergeIncomingSavedObjects: Bool = true
 
 	deinit {
@@ -52,6 +67,12 @@ public final class RTCoreDataStack {
 
 fileprivate typealias Setup = RTCoreDataStack
 fileprivate extension Setup {
+
+	/// Sets up the the whole stack, giving you full control over what model to use and where the resulting file should be.
+	///
+	/// - parameter dataModelName: String representing the name (without extension) of the model file to use. If not supplied,
+	/// - parameter storeURL: Full URL where to create the .sqlite file. Must include the file at the end as well (can't be just directory). If not supplied, user's Documents directory will be used + alphanumerics from app's name. Possible use: when you want to setup the store file into completely custom location. Like say shared container in App Group.
+	/// - parameter callback: A block to call once setup is completed. RTCoreDataStack.isReady is set to true before callback is executed.
 	func setup(withDataModelNamed dataModelName: String? = nil, storeURL: URL? = nil, callback: Callback = {_ in}) {
 
 		let url: URL
@@ -92,6 +113,10 @@ fileprivate extension Setup {
 		callback()
 	}
 
+	/// Attach the persistent stores to the supplied Persistent Store Coordinator.
+	///
+	/// - parameter psc:         Instance of PSC
+	/// - parameter postConnect: Optional closure to execute after successful add (of the stores)
 	func connectStores(toCoordinator psc: NSPersistentStoreCoordinator, andExecute postConnect: (()-> Void)? = nil) {
 		if #available(iOS 10.0, *) {
 			psc.addPersistentStore(with: storeDescription, completionHandler: { [unowned self] (sd, error) in
@@ -140,6 +165,7 @@ fileprivate extension Setup {
 		return sd
 	}
 
+	/// Returns URL for the user's Documents folder
 	var defaultStoreURL: URL {
 		guard let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
 			let log = String(format: "E | %@:%@/%@ Could not fetch Documents directory",
@@ -149,6 +175,7 @@ fileprivate extension Setup {
 		return documentsURL
 	}
 
+	/// Returns String representing only alphanumerics from app's name
 	var cleanAppName: String {
 		guard let appName = Bundle.main.object(forInfoDictionaryKey: "CFBundleName") as? String else {
 			let log = String(format: "E | %@:%@/%@ Unable to fetch CFBundleName from main bundle",
@@ -158,6 +185,11 @@ fileprivate extension Setup {
 		return appName.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
 	}
 
+	/// Instantiates NSManagedObjectModel. If it can't create one, it will crash the app
+	///
+	/// - parameter name: optional name of the Model file. Useful when you want to creates two stacks and copy data between them
+	///
+	/// - returns: NSManagedObjectModel instance, ready to create PSC
 	func managedObjectModel(named name: String? = nil) -> NSManagedObjectModel {
 		if name == nil {
 			guard let mom = NSManagedObjectModel.mergedModel(from: nil) else {
@@ -188,6 +220,7 @@ fileprivate extension Setup {
 fileprivate typealias Notifications = RTCoreDataStack
 fileprivate extension Notifications {
 
+	//	Subscribe the stack to any context's DidSaveNotification
 	func setupNotifications() {
 
 		NotificationCenter.default.addObserver(self,
@@ -196,6 +229,9 @@ fileprivate extension Notifications {
 		                                       object: nil)
 	}
 
+	/// Automatically merges all new, deleted and changed objects from background importerContexts into the mainContext
+	///
+	/// - parameter notification: must be NSManagedObjectContextDidSave notification
 	@objc func handle(notification: Notification) {
 		if !shouldMergeIncomingSavedObjects { return }
 
@@ -229,6 +265,9 @@ fileprivate extension Notifications {
 
 fileprivate typealias Contexts = RTCoreDataStack
 public extension Contexts {
+	/// Importer MOC is your best path to import large amounts of data in the background. Its `mergePolicy` is set to favor objects in memory versus those in the store, thus in case of conflicts newly imported data will trump whatever is on disk.
+	///
+	/// - returns: Newly created MOC with concurrency=NSPrivateQueueConcurrencyType and mergePolicy=NSMergeByPropertyObjectTrumpMergePolicy
 	func importerContext() -> NSManagedObjectContext {
 		let moc = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
 		moc.persistentStoreCoordinator = writerCoordinator
@@ -236,6 +275,9 @@ public extension Contexts {
 		return moc
 	}
 
+	/// Use temporary MOC is for cases where you need short-lived managed objects. Whatever you do in here is never saved, as its `mergePolicy` is set to NSRollbackMergePolicy. Which means all `save()` calls will silently fail
+	///
+	/// - returns: Newly created MOC with concurrency=NSPrivateQueueConcurrencyType and mergePolicy=NSRollbackMergePolicy, with the same PSC as `mainContext`
 	func temporaryContext() -> NSManagedObjectContext {
 		let moc = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
 		moc.persistentStoreCoordinator = mainCoordinator
@@ -243,6 +285,12 @@ public extension Contexts {
 		return moc
 	}
 
+	/// Use this MOC for all cases where you need to allow the customer to create new objects that will be saved to disk. For example, to "add new" / "edit existing" contact in contact management app.
+	///
+	/// It is always set to use mainContext as its `parentContext`, so any saves are transfered to the `mainContext` and thus available to the UI.
+	/// You must make sure that `mainContext` is not read-only when calling this method (assert is run and if it is read-only your app will crash).
+	///
+	/// - returns: Newly created MOC with concurrency=NSPrivateQueueConcurrencyType and mergePolicy=NSMergeByPropertyObjectTrumpMergePolicy and parentContext=mainManagedObjectContext
 	func editorContext() -> NSManagedObjectContext {
 		if isMainContextReadOnly {
 			let log = String(format: "E | %@:%@/%@ Can't create editorContext when isMainContextReadOnly=true.\nHint: you can set it temporary to false, make the changes, save them using save(callback:) and revert to true inside the callback block.",
