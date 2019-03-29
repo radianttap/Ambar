2f77796d4098f4167603a41ee842490982084a05
Aleksandar Vacić <av@radianttap.com>
2019-03-29 18:32:16 +0100

    Experimental ability when saving
    Added parameter to not propagate to parent MOCs.
    Also, perform async saves in main thread, otherwise do sync saves.

--- Ambar/NSManagedObjectContext-Extensions.swift
+++ Ambar/NSManagedObjectContext-Extensions.swift
@@ -31,38 +31,52 @@ public extension NSManagedObjectContext {
 
 		throw CoreDataError.readFailed
 	}
+
+
+
 	/// Performs save on the given context. Automatically saves its parentContext if available.
 	///	If any errors occur, it will return them in the optional callback.
 	///
 	///	Thus you can simply call this as `moc.save()`
 	///
+	/// - parameter shouldPropagate: if `true`, save will propagate through parentContexts chain all the way to the store; otherwise it just save the current context. Default is `true`.
 	/// - parameter callback: closure to be informed about possible errors during save. Or simply as pingback so you know where the save is completed.
 	///
-	func save(_ callback: @escaping (CoreDataError?) -> Void = {_ in}) {
-		if !self.hasChanges {
+	func save(shouldPropagate propagated: Bool = true, callback: @escaping (CoreDataError?) -> Void = {_ in}) {
+		if !hasChanges {
 			callback(nil)
 			return
 		}
 
-		//	async save, to not block the thread it's called on
-		self.performAndWait {
-			do {
-				try self.save()
-
-				//	if there's a parentContext, save that one too
-				if let parentContext = self.parent {
-					parentContext.save(callback)
-					return;
-				}
-
-				callback(nil)
-
-			} catch(let error) {
-//				let log = String(format: "E | %@:%@/%@ Error saving context:\n%@",
-//				                 String(describing: self), #file, #line, error.localizedDescription)
-//				print(log)
-				callback( CoreDataError.saveFailed(error) )
+		if concurrencyType == .mainQueueConcurrencyType {
+			//	in main MOC, perform async save, to avoid blocking the app
+			perform {
+				[unowned self] in
+				self.actualSave(shouldPropagate: propagated, callback: callback)
 			}
+		} else {
+			//	in background MOCs, perform sync save
+			performAndWait {
+				[unowned self] in
+				self.actualSave(shouldPropagate: propagated, callback: callback)
+			}
+		}
+	}
+
+	private func actualSave(shouldPropagate propagated: Bool = true, callback: @escaping (CoreDataError?) -> Void = {_ in}) {
+		do {
+			try self.save()
+
+			//	if there's a parentContext, save that one too
+			if let parentContext = self.parent, propagated {
+				parentContext.save(shouldPropagate: propagated, callback: callback)
+				return
+			}
+
+			callback(nil)
+
+		} catch let error {
+			callback( CoreDataError.saveFailed(error) )
 		}
 	}
 }
