3e8824430780efc9f180e78152c57dbcd04bde51
Aleksandar Vacić <av@radianttap.com>
2024-04-13 20:15:16 +0200

    Removed comments

--- Package.swift
+++ Package.swift
@@ -13,14 +13,11 @@ let package = Package(
 		.visionOS(.v1)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Ambar",
             targets: ["Ambar"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Ambar",
 			dependencies: [],
