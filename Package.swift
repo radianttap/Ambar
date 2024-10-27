fed84467b6d1fc345e45f740edfb76ccc4675137
Aleksandar Vacić <av@radianttap.com>
2024-10-27 10:38:21 +0100

    Swift 6 language mode includes strict-concurrency, so feature flag is not needed

--- Package.swift
+++ Package.swift
@@ -20,10 +20,7 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "Ambar",
-			swiftSettings: [
-				.enableExperimentalFeature("StrictConcurrency")
-			]
+            name: "Ambar"
 		)
 	],
 	swiftLanguageModes: [.v6]
